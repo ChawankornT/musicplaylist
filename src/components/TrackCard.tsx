d76486ad5a8fbd76634265706127b45fd76ebe22
ChawankornT <chawankorn_earthyes@hotmail.com>
2025-09-22 03:17:35 +0700

    add TrackCard

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { FiPlus, FiMinus } from "react-icons/fi";
+
+interface TrackCardProps {
+  track: any;
+  onAddTrack?: (t: any) => void;
+  onDeleteTrack?: (t: any) => void;
+}
 export default function TrackCard({
   track,
-  onAdd,
-}: {
-  track: any;
-  onAdd: (t: any) => void;
-}) {
+  onAddTrack,
+  onDeleteTrack,
+}: TrackCardProps) {
   return (
     <div className="flex items-center gap-3 p-2 rounded hover:bg-gray-300/50">
       <img
@@ -15,19 +20,36 @@ export default function TrackCard({
         className="w-16 h-16 rounded-md"
       />
       <div className="flex-1">
-        <div className="font-medium">{track.trackName}</div>
-        <div className="text-sm text-muted">{track.artistName}</div>
+        <div className="text-md font-semibold">{track.trackName}</div>
+        <div className="text-sm">{track.artistName}</div>
       </div>
+
       <div className="flex gap-2">
         {track.previewUrl && (
           <audio controls src={track.previewUrl} className="w-100" />
         )}
-        <button
-          onClick={() => onAdd(track)}
-          className="px-3 py-1 text-white rounded bg-blue-600 hover:bg-blue-700 cursor-pointer"
-        >
-          Add
-        </button>
+
+        {onAddTrack && !onDeleteTrack && (
+          <div className="flex items-center">
+            <button
+              onClick={() => onAddTrack(track)}
+              className="p-1 text-white bg-blue-400 hover:bg-blue-500 rounded-full cursor-pointer"
+            >
+              <FiPlus size={24} />
+            </button>
+          </div>
+        )}
+
+        {onDeleteTrack && !onAddTrack && (
+          <div className="flex items-center">
+            <button
+              onClick={() => onDeleteTrack(track)}
+              className="p-1 text-white bg-red-300 hover:bg-red-400 rounded-full cursor-pointer"
+            >
+              <FiMinus size={24} />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
